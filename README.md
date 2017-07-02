b16b76596cc800c97004f1012b0a32d079a866d1
guangqiang.t <guangqiang.t@qq.com>
2017-07-02 15:17:12 +0800

    修正了stm32f4xx_conf.h，增加了参数检查

---
+++ README.md
@@ -0,0 +1,2 @@
+# stm32f407-project-template-mdk-v1.8
+## stm32f407工程模板，含串口程序，延时程序
--- User/main.c
+++ User/main.c
@@ -7,10 +7,35 @@ int main(void)
 	int i=0;
 	usart1_config();
 	delay_init();
+//	RCC_AHB1PeriphClockCmd(RCC_APB2Periph_SPI4, ENABLE);
 	while(1)
 	{
 		delay_ms(1000);
 		printf("this is stm32f407 send %d\r\n",i++);
 	}
-	return 0;
+//	return 0;
 }
+
+
+
+//-------------------------------------------------------------------------//
+#ifdef USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param file: pointer to the source file name
+  * @param line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number, */
+  printf("Wrong parameters value: file %s on line %d\r\n", file, line);
+
+  /* Infinite loop */
+  while (1)
+  {
+		;
+  }
+}
+#endif
